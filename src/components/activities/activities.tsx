47fa75a8d1057d988d72248e7d15253277e6d328
Kunal Singh <kunalsin9h@gmail.com>
2023-08-25 00:52:13 +0530

    addin nvim

--- src/components/activities/activities.tsx
+++ src/components/activities/activities.tsx
@@ -3,6 +3,9 @@ import { NeoVim, Spotify, VsCode } from "./types";
 import { Button } from "../ui/button";
 import Link from "next/link";
 
+/*
+Most of VsCodeBox is similar to NeoVimBox code
+*/
 export function VsCodeBox({ act }: { act: VsCode }) {
   const repo = `https://github.com/kunalsin9h/${act.state.replaceAll(
     "Workspace: ",
@@ -48,8 +51,47 @@ export function VsCodeBox({ act }: { act: VsCode }) {
   );
 }
 
-export function NeoVimBox({ act }: { act: NeoVim }) {
-  return <p>{act.name}</p>;
+export async function NeoVimBox({ act }: { act: NeoVim }) {
+  const folderName = act.details.replaceAll("In workspace ", "").split(" ")[0];
+
+  const repo = `https://github.com/kunalsin9h/${folderName}`;
+  const repoExist = hasGithubRepo(repo);
+
+  return (
+    <div className="flex flex-col p-4 w-full">
+      <div className="flex items-center justify-start space-x-8">
+        <Image
+          className=""
+          src="/images/vim.png"
+          alt="Vim Logo"
+          width={80}
+          height={80}
+        />
+        <div>
+          <div className="font-bold">{act.name}</div>
+          <div className="text-sm opacity-80">{act.details}</div>
+          <div className="text-sm opacity-80">{act.state}</div>
+          <div className="text-sm opacity-80">
+            {elapsedTime(act.timestamps.start)} elapsed
+          </div>
+        </div>
+      </div>
+      <Link
+        href={repo}
+        className={`${hasGithubRepo(repo)
+          .then((res) => {
+            if (res) return "";
+            else return "hidden";
+          })
+          .catch((_) => {
+            return "hidden";
+          })} w-full my-2`}
+        target="_black"
+      >
+        <Button className="w-full">View Repository</Button>
+      </Link>
+    </div>
+  );
 }
 
 export function SpotifyBox({ act }: { act: Spotify }) {
@@ -58,6 +100,7 @@ export function SpotifyBox({ act }: { act: Spotify }) {
 
 async function hasGithubRepo(repo: string): Promise<boolean> {
   const response = await fetch(repo);
+  console.log(response.status);
   return response.status === 200;
 }
 
