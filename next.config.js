38c51b423fdaf9aa447c8e0d1b17a572c2525d31
Kunal Singh <kunalsin9h@gmail.com>
2024-11-10 16:57:07 +0530

    fix: svg as static assert

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
   images: {
     remotePatterns: [
       {
